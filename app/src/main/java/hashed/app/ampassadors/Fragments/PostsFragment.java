e11b2abd383bc05402f52f522036a6a364e01884
Ayman2199 <aboessam12345@gmail.com>
2021-03-18 02:13:14 +0200

    ملفات النصوص

--- app/src/main/java/hashed/app/ampassadors/Fragments/PostsFragment.java
+++ app/src/main/java/hashed/app/ampassadors/Fragments/PostsFragment.java
@@ -132,10 +132,10 @@ public class PostsFragment extends Fragment implements Toolbar.OnMenuItemClickLi
                     R.drawable.notification_indicator_icon:
                     R.drawable.notification_icon);
 
-    if (GlobalVariables.getRole().equals("Admin") || GlobalVariables.getRole().equals("Publisher")){
-
-      floatingButton.setVisibility(View.VISIBLE);
-    }
+//    if (GlobalVariables.getRole().equals("Admin") || GlobalVariables.getRole().equals("Publisher")){
+//
+//      floatingButton.setVisibility(View.VISIBLE);
+//    }
 
 
 
