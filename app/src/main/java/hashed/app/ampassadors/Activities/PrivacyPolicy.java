8474297510ce2bc6364d8ffc0b82ae513e1f0eaa
Ayman2199 <aboessam12345@gmail.com>
2021-03-21 02:35:53 +0200

    تعديل ع التصميم

--- app/src/main/java/hashed/app/ampassadors/Activities/PrivacyPolicy.java
+++ app/src/main/java/hashed/app/ampassadors/Activities/PrivacyPolicy.java
@@ -1,18 +1,33 @@
 package hashed.app.ampassadors.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import hashed.app.ampassadors.R;
 
-public class PrivacyPolicy extends AppCompatActivity {
+public class PrivacyPolicy extends AppCompatActivity implements Toolbar.OnMenuItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privacy_policy);
 
+        setUpToolBarAndActions();
+    }
 
+    private void setUpToolBarAndActions() {
+
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setNavigationOnClickListener(v -> onBackPressed());
+        toolbar.setOnMenuItemClickListener(this);
+
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        return false;
     }
 }
