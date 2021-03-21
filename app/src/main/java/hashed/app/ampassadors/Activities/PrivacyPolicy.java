3c13f6669c3812a29c033fa4bd0b361d88f62f2f
Ayman2199 <aboessam12345@gmail.com>
2021-03-21 04:12:42 +0200

    سس

--- app/src/main/java/hashed/app/ampassadors/Activities/PrivacyPolicy.java
+++ app/src/main/java/hashed/app/ampassadors/Activities/PrivacyPolicy.java
@@ -8,29 +8,16 @@ import android.view.MenuItem;
 
 import hashed.app.ampassadors.R;
 
-public class PrivacyPolicy extends AppCompatActivity implements Toolbar.OnMenuItemClickListener {
+public class PrivacyPolicy extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privacy_policy);
 
-        setUpToolBarAndActions();
-    }
-
-    private void setUpToolBarAndActions() {
-
-        final Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setNavigationOnClickListener(v -> onBackPressed());
-        toolbar.setOnMenuItemClickListener(this);
-
         final Toolbar toolbar = findViewById(R.id.about_toolbar);
         toolbar.setNavigationOnClickListener(v -> onBackPressed());
-
     }
 
-    @Override
-    public boolean onMenuItemClick(MenuItem item) {
-        return false;
-    }
+
 }
